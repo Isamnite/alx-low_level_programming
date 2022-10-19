4ad8d44184de71c8ae508f6edb4cb2998b1539ce
Isamnite <isamnite31@gmail.com>
2022-10-19 01:18:31 -0700

    okay 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - A function that prints the alphabet
+ *
+ * Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+	char i, j;
+
+	j = 0;
+
+	while (j < 10)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		j++;
+		_putchar('\n');
+	}
+}
