7218d50b22443290485fd92e2f03dadcf305fe29
Isamnite <isamnite31@gmail.com>
2022-10-20 13:43:33 -0700

    e no easy

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,20 +1,16 @@
 #include "main.h"
-/**
-* print_triangle - prints a triangle
-* @size: takes in a integer for size of triangle
-*/
-void print_triangle(int size)
-{
-	int row, column, k;
+#include <stdio.h>
 
-	if (size <= 0)
-		_putchar('\n');
-	for (row = 0; row < size; row++)
-	{
-		for (column = size - row; column > 1; column--)
-			_putchar(' ');
-		for (k = row + column; k >= 1; k--)
-			_putchar('#');
-		_putchar('\n');
-	}
+/**
+* mul - function that multiplies two integers
+* @a: First integer
+* @b: Second integer
+* Return: mul.
+*/
+int mul(int a, int b)
+{
+	int x;
+
+	x = a * b;
+return (x);
 }
